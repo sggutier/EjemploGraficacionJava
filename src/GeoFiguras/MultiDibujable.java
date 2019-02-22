460104fc2f495bc86174489a27cdabf301e53af5
Germán Gutiérrez <me@sggc.me>
2019-02-22 10:29:06 -0600

    Correjido bug con transformaciones de multidibujable

--- src/GeoFiguras/MultiDibujable.java
+++ src/GeoFiguras/MultiDibujable.java
@@ -8,6 +8,7 @@ import java.util.Collection;
 
 public class MultiDibujable extends ArrayList<Dibujable> implements Dibujable {
     public MultiDibujable() {
+        super();
     }
 
     public MultiDibujable(Collection<? extends Dibujable> collection) {
@@ -22,33 +23,33 @@ public class MultiDibujable extends ArrayList<Dibujable> implements Dibujable {
 
     @Override
     public Dibujable trasladar(double dx, double dy) {
-        MultiDibujable ans = new MultiDibujable(this);
-        for(Dibujable d : ans)
-            d.trasladar(dx, dy);
+        MultiDibujable ans = new MultiDibujable();
+        for(Dibujable d : this)
+            ans.add(d.trasladar(dx, dy));
         return ans;
     }
 
     @Override
     public Dibujable rotar(double deg) {
-        MultiDibujable ans = new MultiDibujable(this);
-        for(Dibujable d : ans)
-            d.rotar(deg);
+        MultiDibujable ans = new MultiDibujable();
+        for(Dibujable d : this)
+            ans.add(d.rotar(deg));
         return ans;
     }
 
     @Override
     public Dibujable escalar(double fx, double fy) {
-        MultiDibujable ans = new MultiDibujable(this);
-        for(Dibujable d : ans)
-            d.escalar(fx, fy);
+        MultiDibujable ans = new MultiDibujable();
+        for(Dibujable d : this)
+            ans.add(d.escalar(fx, fy));
         return ans;
     }
 
     @Override
     public Dibujable sizallar(double a, double b) {
-        MultiDibujable ans = new MultiDibujable(this);
-        for(Dibujable d : ans)
-            d.sizallar(a, b);
+        MultiDibujable ans = new MultiDibujable();
+        for(Dibujable d : this)
+            ans.add(d.sizallar(a, b));
         return ans;
     }
 }
